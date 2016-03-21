33bdc3685ad42f440c1abc32fe99c7a3deffa35b
sirichandana24 <sirichandana24@missionrnd.com>
2016-03-21 12:29:47 +0530

    Submit Commit

--- src/mergeSortedArrays.cpp
+++ src/mergeSortedArrays.cpp
@@ -21,7 +21,58 @@ struct transaction {
 	char date[11];
 	char description[20];
 };
-
+int check_date(char*,char*);
 struct transaction * mergeSortedArrays(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+	if (A == NULL || B==NULL)
+		return NULL;
+	if (ALen < 0 || BLen < 0)
+		return NULL;
+	int i = 0, j = 0, k = 0,res;
+	struct transaction *temp;
+	char *temp1=NULL, *temp2=NULL;
+	temp = (struct transaction*)malloc((ALen + BLen)*sizeof(struct transaction));
+	while (i < ALen && j < BLen)
+	{
+		res = check_date(A[i].date, B[j].date);
+		if (res == -1)
+			temp[k++] = A[i++];
+		else
+			temp[k++] = B[j++];
+	}
+	while (i < ALen)
+		temp[k++] = A[i++];
+	while (j < BLen)
+		temp[k++] = B[j++];
+	return temp;
+
+}
+int check_date(char *temp1, char *temp2)
+{
+	int d1=0, m1=0, y1=0, d2=0, m2=0, y2=0;
+	d1 = (temp1[0] - '0') * 10 + (temp1[1]-'0');
+	d2 = (temp2[0] - '0') * 10 + (temp2[1]-'0');
+	m1 = (temp1[3] - '0') * 10 + (temp1[4]-'0');
+	m2 = (temp2[3] - '0') * 10 + (temp2[4]-'0');
+	y1 = (temp1[6] - '0') * 1000 + (temp1[7] - '0') * 100 + (temp1[8] - '0') * 10 + (temp1[9] - '0');
+	y2 = (temp2[6] - '0') * 1000 + (temp2[7] - '0') * 100 + (temp2[8] - '0') * 10 + (temp2[9] - '0');
+	if (y1 == y2 && m1 == m2 && d1 == d2)
+		return 1;
+	if (y1 < y2)
+		return -1;
+	if (y1 > y2)
+		return 1;
+	if (y1 == y2)
+	{
+		if (m1 < m2)
+			return -1;
+		else if (m1 == m2)
+		{
+			if (d1 < d2)
+				return -1;
+			else
+				return 1;
+		}
+		else
+			return 1;
+	}
 }
